2656fe275fa459c599a711376d3dbaac25527c25
Abhinav <abhinav@AVs-Mac.local>
2024-02-25 23:25:22 +0530

    [25 feb 2024] introducing express, basic express server, restful apis

---
+++ express_basics/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+// new application
+const app = express();
+
+const products = [
+    {id: "101", name: "Shirt", category: "Clothing", price: 2000},
+    {id: "102", name: "saree", category: "Clothing", price: 4000},
+    {id: "103", name: "watch", category: "accessories", price: 3000},
+]
+
+// create a get endpoint and execute any cb when a req is received
+app.get("/", (req, res) => {
+    res.send("<h1>Home Page</h1>");
+})
+
+app.post("/", (req, res) => {
+    res.status(201);
+    res.send("created success")
+})
+
+app.get("/product", (req, res) => {
+    // const data = JSON.stringify(products);
+    res.send(products);
+})
+
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`express server running at ${PORT}`);
+})
